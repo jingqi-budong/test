b2a6b99a84212b77f205aa7ce650e72e0141a935
jingqi-budong <2362712780@qq.com>
2018-06-14 23:13:02 +0800

    Update user_12306.py

--- user_12306.py
+++ user_12306.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Time    : 2017/8/30 19:15
-# @Author  : 强子
+# @Author  : 
 # @File    : user_12306.py
 # @Software: PyCharm
-user = '413401513@qq.com'
-password = 'jingqi_python'
+user = '账号'
+password = '密码'
